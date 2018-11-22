4b89dc9446cf63cca422bff9438751c9f0103fd6
Mathieu Ribot <mribot@hotmail.fr>
2018-11-22 18:16:36 +0100

    challenge#2 try4 -front work in progress- day 27

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import 'bootstrap';
+
+// Makes the dropdown of the navbar working //
+// See https://stackoverflow.com/questions/22383547/bootstrap-dropdown-menu-is-not-working //
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
